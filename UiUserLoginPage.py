bbfe2849a21d8845c084da9d231a52c74ea4c3b9
SUNIL SHARMA <sharma.sunil12527@gmail.com>
2024-05-31 01:15:01 +0530

    update

--- UiUserLoginPage.py
+++ UiUserLoginPage.py
@@ -64,7 +64,7 @@ def authenticate_with_password(root):  # Add root as an argument
 
 
 
-def fingerPrint_connection(root, username):
+def fingerPrint_connection(root):
     from fingerprint import FingerPrint
     print("**** [option selected] Finger print option selected \n")
     # root.withdraw()  # Hide the main window 
